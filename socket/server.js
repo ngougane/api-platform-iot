4a9ce543c380837f8368b88a68498e9a5b05ebe7
Vlad <h.debelsunce@hotmail.fr>
2020-03-18 10:44:23 +0100

    create branche

--- socket/server.js
+++ socket/server.js
@@ -8,7 +8,7 @@ var xbeeAPI = new xbee_api.XBeeAPI({
 });
 
 
-let serialport = new SerialPort("/dev/ttyUSB1", {
+let serialport = new SerialPort("/dev/tty.SLAB_USBtoUART", {
   baudRate: 9600,
 }, function (err) {
   if (err) {
@@ -67,7 +67,7 @@ xbeeAPI.parser.on("data", function (frame) {
 
 
   } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
-    
+
   } else {
     console.debug(frame);
     let dataReceived = String.fromCharCode.apply(null, frame.commandData)
